fa987419aea753a75d325297bea6bd22718307ad
Hamza Talhaouia <49211881+hamzatlh@users.noreply.github.com>
2022-12-23 23:59:10 +0100

    Add files via upload

--- move_player.c
+++ move_player.c
@@ -6,7 +6,7 @@
 /*   By: htalhaou <htalhaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 22:06:44 by htalhaou          #+#    #+#             */
-/*   Updated: 2022/12/23 01:40:44 by htalhaou         ###   ########.fr       */
+/*   Updated: 2022/12/23 23:00:47 by htalhaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,86 +15,94 @@
 
 void ft_move_up(t_game *game)
 {
-	if (game->map.map[game->map.player.x][game->map.player.y] == 'P')
-	{	
-		if (game->map.map[game->map.player.x - 1][game->map.player.y] != '1')
+	if (game->map.map[game->map.player.x - 1][game->map.player.y] != '1' && game->map.map[game->map.player.x - 1][game->map.player.y] != 'E')
+		{
+			if (game->map.map[game->map.player.x - 1][game->map.player.y] == '0')
 			{
 				game->map.map[game->map.player.x][game->map.player.y] = '0';
 				game->map.map[game->map.player.x - 1][game->map.player.y] = 'P';
 				game->map.player.x--;
 			}
-		else if (game->map.map[game->map.player.x - 1][game->map.player.y] == 'C')
-				{
-					game->map.map[game->map.player.x][game->map.player.y] = '0';
-					game->map.map[game->map.player.x - 1][game->map.player.y] = 'P';
-					game->map.player.x--;
-				}
-		mlx_clear_window(game->mlx, game->win);
+			else if (game->map.map[game->map.player.x - 1][game->map.player.y] == 'C')
+			{
+				game->count_C--;
+				game->map.map[game->map.player.x][game->map.player.y] = '0';
+				game->map.map[game->map.player.x - 1][game->map.player.y] = 'P';
+				game->map.player.x--;
+				ft_printf("%d\n", game->count_C);
+			}
+		backround(game);
 		put_elements(game);
-	}
+		}
 }
 
 void ft_move_down(t_game *game)
 {
-	if (game->map.map[game->map.player.x][game->map.player.y] == 'P')
-	{	
-		if (game->map.map[game->map.player.x + 1][game->map.player.y] != '1')
+	if (game->map.map[game->map.player.x + 1][game->map.player.y] != '1' && game->map.map[game->map.player.x + 1][game->map.player.y] != 'E')
+		{
+			if (game->map.map[game->map.player.x + 1][game->map.player.y] == '0')
 			{
 				game->map.map[game->map.player.x][game->map.player.y] = '0';
 				game->map.map[game->map.player.x + 1][game->map.player.y] = 'P';
 				game->map.player.x++;
 			}
-		else if (game->map.map[game->map.player.x - 1][game->map.player.y] == 'C')
-					{
-						game->map.map[game->map.player.x][game->map.player.y] = '0';
-						game->map.map[game->map.player.x - 1][game->map.player.y] = 'P';
-						game->map.player.x++;
-					}
-		mlx_clear_window(game->mlx, game->win);
+			else if (game->map.map[game->map.player.x + 1][game->map.player.y] == 'C')
+				{
+					game->count_C--;
+					game->map.map[game->map.player.x][game->map.player.y] = '0';
+					game->map.map[game->map.player.x + 1][game->map.player.y] = 'P';
+					game->map.player.x++;
+					ft_printf("%d\n", game->count_C);
+				}
+		backround(game);
 		put_elements(game);
-	}
+		}
 }
 
 void ft_move_left(t_game *game)
 {
-	if (game->map.map[game->map.player.x][game->map.player.y] == 'P')
-	{
-		if (game->map.map[game->map.player.x][game->map.player.y - 1] != '1')
+	if (game->map.map[game->map.player.x][game->map.player.y - 1] != '1' && game->map.map[game->map.player.x][game->map.player.y - 1] != 'E')
+		{
+			if (game->map.map[game->map.player.x][game->map.player.y - 1] == '0')
 			{
 				game->map.map[game->map.player.x][game->map.player.y] = '0';
 				game->map.map[game->map.player.x][game->map.player.y - 1] = 'P';
 				game->map.player.y--;
 			}
-		else if (game->map.map[game->map.player.x - 1][game->map.player.y] == 'C')
-					{
-						game->map.map[game->map.player.x][game->map.player.y] = '0';
-						game->map.map[game->map.player.x - 1][game->map.player.y] = 'P';
-						game->map.player.y--;
-					}
-		mlx_clear_window(game->mlx, game->win);
-		put_elements(game);
-	}
+			else if (game->map.map[game->map.player.x][game->map.player.y - 1] == 'C')
+				{
+					game->count_C--;
+					game->map.map[game->map.player.x][game->map.player.y] = '0';
+					game->map.map[game->map.player.x][game->map.player.y - 1] = 'P';
+					game->map.player.y--;
+					ft_printf("%d\n", game->count_C);
+				}
+			backround(game);
+			put_elements(game);
+		}
 }
 
 void ft_move_right(t_game *game)
 {
-	if (game->map.map[game->map.player.x][game->map.player.y] == 'P')
-	{
-		if (game->map.map[game->map.player.x][game->map.player.y + 1] != '1')
+	if (game->map.map[game->map.player.x][game->map.player.y + 1] != '1' && game->map.map[game->map.player.x][game->map.player.y + 1] != 'E')
+		{
+			if (game->map.map[game->map.player.x][game->map.player.y + 1] == '0')
 			{
 				game->map.map[game->map.player.x][game->map.player.y] = '0';
 				game->map.map[game->map.player.x][game->map.player.y + 1] = 'P';
 				game->map.player.y++;
 			}
-		else if (game->map.map[game->map.player.x - 1][game->map.player.y] == 'C')
-					{
-						game->map.map[game->map.player.x][game->map.player.y] = '0';
-						game->map.map[game->map.player.x - 1][game->map.player.y] = 'P';
-						game->map.player.y++;
-					}
-		mlx_clear_window(game->mlx, game->win);
-		put_elements(game);
-	}
+			else if (game->map.map[game->map.player.x][game->map.player.y + 1] == 'C')
+				{
+					game->count_C--;
+					game->map.map[game->map.player.x][game->map.player.y] = '0';
+					game->map.map[game->map.player.x][game->map.player.y + 1] = 'P';
+					game->map.player.y++;
+					ft_printf("%d\n", game->count_C);
+				}
+			backround(game);
+			put_elements(game);
+		}
 }
 
 int	ft_keypress(int key, t_game *game)
